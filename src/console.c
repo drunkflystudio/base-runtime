a176f94e6df9ab7b9f28d128f39fffb2844b14e5
Drunk Fly Team <drunkflyteam@gmail.com>
2025-03-02 00:18:22 +0200

    Implemented test static method.

--- src/console.c
+++ src/console.c
@@ -1,1 +1,8 @@
 #include <drunkfly/vm.h>
+#include <stdarg.h>
+
+void Console_print(lua_State* L, va_list args)
+{
+    const char* str = va_arg(args, const char*);
+    fprintf(stderr, "Console_print: %s\n", str);
+}
